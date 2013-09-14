f8ee6f2fa572497724b62b56d7ea59018067617f
Matti Tuomola / Labris <matti.tuomola@lasoft.fi>
2013-09-14 19:57:10 +0300

    Stared AI and pathfinding.
    Signed-off-by: Matti Tuomola / Labris <matti.tuomola@lasoft.fi>

---
+++ 24hgame1/AI/Properties/AIenums.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace hgame1.AI.Properties
+{
+    public enum AIstate
+    {
+        On,
+        Off
+    }
+
+    public enum AItype
+    {
+        Hostile,
+        Passive,
+        Friendly
+    }
+}
