31cb9a5a9d14b88ade9836edaf7ad3a4c1b4d458
Lasse Numminen <lasse.numminen@lasoft.fi>
2013-09-15 01:42:35 +0300

    Added update and rendering logic to particle engine

--- 24hgame1/Graphics/Particles/ParticleEngine.cs
+++ 24hgame1/Graphics/Particles/ParticleEngine.cs
@@ -1,5 +1,8 @@
 using System;
 using System.Collections.Generic;
+using OpenTK;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
 
 namespace hgame1.Graphics.Particles
 {
@@ -7,9 +10,45 @@ namespace hgame1.Graphics.Particles
 	{
 
 		public static List<Particle> Particles = new List<Particle>();
+		public static List<ParticleEmitter> Emitters = new List<ParticleEmitter>();
 
+		public static void Init(GameWindow gw)
+		{
+			gw.RenderFrame += HandleRenderFrame;
+			gw.UpdateFrame += HandleUpdateFrame;
+		}
 
+		static void HandleRenderFrame (object sender, FrameEventArgs e)
+		{
+			foreach (var particle in Particles) {
+				particle.Draw ();
+			}
+		}
 
+		static void HandleUpdateFrame (object sender, FrameEventArgs e)
+		{
+			// Collect dead particles to this stack
+			ConcurrentStack<Particle> dead = new ConcurrentStack<Particle>();
+
+			// Update all particles
+			Parallel.ForEach (Particles, (particle) => {
+				if(!particle.Update (e.Time))
+				{
+					// If it returned false, it's dead
+					dead.Push (particle);
+				}
+			});
+
+			// Remove dead particles
+			foreach (var particle in dead) {
+				Particles.Remove (particle);
+			}
+
+			// Emit new particles
+			foreach (var emitter in Emitters) {
+				Particles.AddRange (emitter.Emit ());
+			}
+		}
 
 
 	}
