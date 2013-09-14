398f37b6b019e425e50e5715ba43837a73a227f7
Matti Tuomola / Labris <matti.tuomola@lasoft.fi>
2013-09-14 18:43:54 +0300

    Started Characters.
    Signed-off-by: Matti Tuomola / Labris <matti.tuomola@lasoft.fi>

---
+++ 24hgame1/CharacterEntities/Character.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using hgame1.CharacterEntities.Properties;
+using OpenTK;
+
+namespace hgame1.CharacterEntities
+{
+    class Character
+    {
+        public CharacterControltype Charactertype { get; private set; }
+
+        public Vector2 CharacterLocation { get; private set; }
+        public float CharacterDirection { get; private set; }
+
+
+
+        public Character()
+        {
+
+        }
+
+        public void setDirection(float _direction)
+        {
+            this.CharacterDirection = _direction;
+        }
+    }
+}
---
+++ 24hgame1/CharacterEntities/Properties/Characterenums.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace hgame1.CharacterEntities.Properties
+{
+    public enum CharacterControltype
+    {
+        Player,
+        NetworkPlayer,
+        AI
+    }
+}
