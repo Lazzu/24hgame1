8fbe9187b6abd9d8d40059b0154079a5e6d2dd26
Matti Tuomola / Labris <matti.tuomola@lasoft.fi>
2013-09-14 20:50:28 +0300

    Added weapons and projectiles. Added Characters to GameArea. implemented pahtfindingmap in tilemap.
    Signed-off-by: Matti Tuomola / Labris <matti.tuomola@lasoft.fi>

---
+++ 24hgame1/Weapons/Projectile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace hgame1.Weapons
+{
+    class Projectile
+    {
+        public Projectile()
+        {
+        }
+    }
+}
