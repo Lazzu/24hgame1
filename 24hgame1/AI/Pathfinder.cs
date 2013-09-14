d991d1a2cde1aebc169b7cdffef032d348a01ba6
Matti Tuomola / Labris <matti.tuomola@lasoft.fi>
2013-09-14 23:28:32 +0300

    Started A*
    Signed-off-by: Matti Tuomola / Labris <matti.tuomola@lasoft.fi>

--- 24hgame1/AI/Pathfinder.cs
+++ 24hgame1/AI/Pathfinder.cs
@@ -2,16 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using OpenTK;
+using hgame1.AI.Properties;
 
 namespace hgame1.AI
 {
-    static class Pathfinder
+    public class Pathfinder
     {
-        static public int[,] PathMap { get; private set; }
+        public int[,] PathMap { get; private set; }
 
-        public static void setPathMap(int[,] _pathmap)
+        public List<Node> closedList;
+        public List<Node> openList;
+
+        public void setPathMap(int[,] _pathmap)
         {
-            PathMap = _pathmap;
+            this.PathMap = _pathmap;
+        }
+
+        public Node TranslateToNode(float _x, float _y)
+        {
+            Node tempNode;
+            tempNode.set_x(_x);
+            tempNode.set_y(_y);
+
+            return tempNode;
+        }
+
+        public List<Node> getPath(Node _startpoint, Node _endpoint)
+        {
+            return this.closedList;
         }
     }
 }
