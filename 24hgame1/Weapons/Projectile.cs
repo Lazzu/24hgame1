d5eef711cc93a05f96302dbf890d1348e9a829c7
Matti Tuomola / Labris <matti.tuomola@lasoft.fi>
2013-09-15 02:43:46 +0300

    Physics start for projectiles
    Signed-off-by: Matti Tuomola / Labris <matti.tuomola@lasoft.fi>

--- 24hgame1/Weapons/Projectile.cs
+++ 24hgame1/Weapons/Projectile.cs
@@ -2,13 +2,43 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using hgame1.Physics;
 
 namespace hgame1.Weapons
 {
-    class Projectile
+    class Projectile : ISimplePhysics
     {
+        private float physicstravelspeed;
+        private float physicsdirection;
+
         public Projectile()
         {
+
         }
+        #region ISimplephysics implementation
+        public float travelspeed
+        {
+            get
+            {
+                return this.physicstravelspeed;
+            }
+            set
+            {
+                this.physicstravelspeed = travelspeed;
+            }
+        }
+
+        public float traveldirection
+        {
+            get
+            {
+                return this.physicsdirection;
+            }
+            set
+            {
+                this.physicsdirection = traveldirection;
+            }
+        }
+        #endregion
     }
 }
