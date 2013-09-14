8497615abd3d2c7fbaa6e44839f204efb3b02d13
Matti Tuomola / Labris <matti.tuomola@lasoft.fi>
2013-09-14 17:43:37 +0300

    Extended tiletypes. Added constructor for each type.

--- 24hgame1/Tilemap/Tile.cs
+++ 24hgame1/Tilemap/Tile.cs
@@ -20,14 +20,69 @@ namespace hgame1.Tilemap
         public Doorstate Tiledoorstate { get; private set; }
         public Doortype Tiledoortype { get; private set; }
         public Doordirection Tiledoordirection { get; private set; }
+        public WindowState Tilewindowstate { get; private set; }
 
-        //basic constructor that greates basictile
+        /// <summary>
+        /// basictile constructor.
+        /// </summary>
         public Tile()
         {
             this.Tileproperty = Tiletype.Floor;
             this.Floortexturename = "basictile";
             this.Walltexturename = "basictile";
             this.Ceilingtexturename = "basictile";
+        }
+        
+        /// <summary>
+        /// Floortile constructor.
+        /// </summary>
+        public Tile(string _floortexture)
+        {
+            this.Tileproperty = Tiletype.Floor;
+            this.Floortexturename = _floortexture;
+        }
+
+        /// <summary>
+        /// Floortile with ceiling constructor.
+        /// </summary>
+        public Tile(string _floortexture, string _ceilingtexture)
+        {
+            this.Tileproperty = Tiletype.FloorCeiling;
+            this.Floortexturename = _floortexture;
+            this.Ceilingtexturename = _ceilingtexture;
+        }
+
+        /// <summary>
+        /// Walltile constructor.
+        /// </summary>
+        public Tile(Walltype _walltype, string _walltexture, string _ceilingtexture)
+        {
+            this.Tileproperty = Tiletype.Wall;
+            this.Tilewalltype = _walltype;
+            this.Walltexturename = _walltexture;
+            this.Ceilingtexturename = _ceilingtexture;
+        }
+
+        /// <summary>
+        /// Wall with window constructor.
+        /// </summary>
+        public Tile(WindowState _windowstate, string _walltexture)
+        {
+            this.Tileproperty = Tiletype.WallWindow;
+            this.Tilewindowstate = _windowstate;
+            this.Walltexturename = _walltexture;
+        }
+
+        /// <summary>
+        /// Door constructor
+        /// </summary>
+        public Tile(Doortype _doortype, Doorstate _doorstate, Doordirection _doordirection, string _floorTexture)
+        {
+            this.Tileproperty = Tiletype.Door;
+            this.Tiledoortype = _doortype;
+            this.Tiledoorstate = _doorstate;
+            this.Tiledoordirection = _doordirection;
+            this.Floortexturename = _floorTexture;
         }
     }
 }
